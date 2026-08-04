c4a95b40e86a12b66e52b72ab006f851a91fe365
Trey Yearick <110922310+fanfanchu@users.noreply.github.com>
2026-08-05 03:14:43 +0800

    Change invite link mode to 'custom'

--- src/config/index.js
+++ src/config/index.js
@@ -449,7 +449,7 @@ export const config  = {
         // 邀请链接配置
         inviteLinkConfig: {
             // 链接模式：'auto'=自动使用当前站点域名，'custom'=使用自定义域名
-            linkMode: 'auto',
+            linkMode: 'custom',
             // 自定义域名，当linkMode为'custom'时使用
             customDomain: 'https://vip.ytjcok.org'
         }
